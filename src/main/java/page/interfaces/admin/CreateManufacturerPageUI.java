6e67cf7172990989f90badf317e54eab2977aada
Nguyễn Ngọc Sơn <nnson0310@gmail.com>
2022-07-15 11:18:00 +0700

    Implement create_manufacturer testcases - admin

--- src/main/java/page/interfaces/admin/CreateManufacturerPageUI.java
+++ src/main/java/page/interfaces/admin/CreateManufacturerPageUI.java
@@ -4,4 +4,8 @@ public abstract class CreateManufacturerPageUI {
     public static final String DESCRIPTION_TEXTBOX_IFRAME = "xpath=//iframe[@id='Description_ifr']";
     public static final String PICTURE_UPLOAD_BUTTON = "css=div.qq-upload-button-selector";
     public static final String PICTURE_UPLOAD_INPUT = "css=div.qq-upload-button-selector input";
+    public static final String PICTURE_UPLOAD_LABEL_SPAN = "xpath=//span[contains(@class, 'qq-upload-file') and text()='%s']";
+    public static final String MOD_SWITCH_BUTTON = "css=span.onoffswitch-switch";
+    public static final String PICTURE_UPLOAD_FORM_DIV = "xpath=//div[contains(@class, 'advanced-setting')]//label[text()='Picture']";
+    public static final String PICTURE_UPLOAD_IMG = "css=div.uploaded-image img";
 }
--- src/main/java/page/objects/admin/CreateManufacturerPage.java
+++ src/main/java/page/objects/admin/CreateManufacturerPage.java
@@ -2,6 +2,7 @@ package page.objects.admin;
 
 import commons.AdminBasePage;
 import commons.GlobalConstants;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import page.interfaces.admin.CreateManufacturerPageUI;
@@ -10,6 +11,7 @@ public class CreateManufacturerPage extends AdminBasePage {
     WebDriver driver;
 
     WebDriverWait explicitWait;
+    JavascriptExecutor jsExecutor;
 
     public CreateManufacturerPage(WebDriver driver) {
         this.driver = driver;
@@ -20,9 +22,22 @@ public class CreateManufacturerPage extends AdminBasePage {
     }
 
     public void uploadManufacturerPicture(WebDriver driver, String fileName) {
-        waitForElementClickable(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_BUTTON);
-        clickToElement(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_BUTTON);
-
+        jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].setAttribute('style', 'font-size: 10px !important')", getElement(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_INPUT));
         sendKeyToElement(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_INPUT, GlobalConstants.getGlobalConstants().getUploadFilePath() + fileName);
+    }
+
+    public boolean isUploadPictureDisplayed(WebDriver driver, String pictureName) {
+        waitForElementVisible(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_LABEL_SPAN, pictureName);
+        waitForElementVisible(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_IMG);
+        return isElementDisplayed(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_LABEL_SPAN, pictureName)
+                && isElementDisplayed(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_IMG);
+    }
+
+    public void clickToModSwitchButton(WebDriver driver) {
+        if (!isElementDisplayed(driver, CreateManufacturerPageUI.PICTURE_UPLOAD_FORM_DIV)) {
+            waitForElementClickable(driver, CreateManufacturerPageUI.MOD_SWITCH_BUTTON);
+            clickToElement(driver, CreateManufacturerPageUI.MOD_SWITCH_BUTTON);
+        }
     }
 }
